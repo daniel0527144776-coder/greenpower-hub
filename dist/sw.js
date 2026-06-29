ece73b526052e102996050e0b097b19246fb0f4a
Daniel Machol <daniel0527144776@gmail.com>
2026-06-29 14:59:50 +0300

    fix: add dist folder + point wrangler to ./dist

---
+++ dist/sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'gp-hub-v31';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetchPromise = fetch(e.request).then(networkResp => {
+        if (networkResp && networkResp.status === 200 && networkResp.type === 'basic') {
+          const respClone = networkResp.clone();
+          caches.open(CACHE).then(c => c.put(e.request, respClone));
+        }
+        return networkResp;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
